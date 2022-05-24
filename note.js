e9773983188b100f46ea75b1661f462e33b6e01d
yeahhhhhhhhhhh <73042510+yeahhhhhhhhhhh@users.noreply.github.com>
2022-05-24 09:31:26 +0800

    Update note.js

--- note.js
+++ note.js
@@ -1,1 +1,41 @@
+https://developer.mozilla.org/en-US/
 
+// basic javascript
+// for loop
+for(let index in cars)
+for(let car of cars)
+for(let i = 0; i < 100; i++)
+
+// String
+newImg.src = `${baseURL}${i}.png`
+
+//shortcut
+duplicate: shift + option + ⬇️: duplicate
+search: shift + command + p
+
+// classList
+XXX.classList.add(“class name”);
+
+// parent and children
+XXX.parentElement
+XXX.children[0]
+XXX.previousElementSibling
+XXX.nextElementSibling
+
+// create things
+document.body.appendChild(XXX)
+document.createElement('xxx')
+xxx.append
+xxx.prepend
+eg.
+const h2 = document.createElement('h2');
+h2.append("yay!") or h2.innerText = 'yay!';
+const h1 = document.querySelector('h1');
+h1.insertAdjacentElement('afterend', h2);
+or
+h1.after(h2);
+
+// remove child
+xxx.remove();
+
+// 
